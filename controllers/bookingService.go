05c8b74997e758b05d7d9bfe1bfbcba4ae4963d6
elmashad <elmashad285@gmail.com>
2019-03-05 00:54:46 +0200

    save

--- controllers/bookingService.go
+++ controllers/bookingService.go
@@ -25,6 +25,8 @@ func (eh *bookingService) AddBookingHandler(w http.ResponseWriter, r *http.Reque
 	fmt.Println("second commit")
 	fmt.Println("second commit")
 	fmt.Println("second commit")
+	fmt.Println("second commit")
+	fmt.Println("second commit")
 
 	//fmt.Fprint(w, `{"id":%d}`, 2222)
 }
